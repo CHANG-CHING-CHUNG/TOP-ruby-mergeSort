e1e85c3ac5758e8285303aa069e85ac8a9498a94
CHANG-CHING-CHUNG <54635661+CHANG-CHING-CHUNG@users.noreply.github.com>
2020-11-19 18:32:14 +0800

    updated

--- mergeSort.rb
+++ mergeSort.rb
@@ -1,29 +1,30 @@
-const unsortedArr = [31, 27, 28, 42, 13, 8, 11, 30, 17, 41, 15, 43, 1, 36, 9, 16, 20, 35, 48, 37, 7, 26, 34, 21, 22, 6, 29, 32, 49, 10, 12, 19, 24, 38, 5, 14, 44, 40, 3, 50, 46, 25, 18, 33, 47, 4, 45, 39, 23, 2];
+unsortedArr = [31, 27, 28, 42, 13, 8, 11, 30, 17, 41, 15, 43, 1, 36, 9, 16, 20, 35, 48, 37, 7, 26, 34, 21, 22, 6, 29, 32, 49, 10, 12, 19, 24, 38, 5, 14, 44, 40, 3, 50, 46, 25, 18, 33, 47, 4, 45, 39, 23, 2];
 
-function mergeSort(numArr) {
-	let result = [];
-	if ( numArr.length === 1) {
+def mergeSort(numArr) 
+  result = [];
+	if numArr.length == 1
   	return numArr;
-  }
-  const halfLength = Math.floor(numArr.length / 2);
-  const leftArr = numArr.slice(0, halfLength);
-  const rightArr = numArr.slice(halfLength);
+  end
+  halfLength = (numArr.length / 2).floor();
+  leftArr = numArr.slice(0, halfLength);
+  rightArr = numArr.slice(halfLength, numArr.length);
 	return (
   	merge(mergeSort(leftArr), mergeSort(rightArr))
   );
-}
+end
 
-function merge(arr1, arr2) {
-	const sorted = [];
-  while(arr1.length && arr2.length) {
-  	if(arr1[0] < arr2[0]) {
+def merge(arr1, arr2) 
+  sorted = [];
+
+  while arr1.length != 0 && arr2.length != 0 do
+  	if arr1[0] < arr2[0] 
     	sorted.push(arr1.shift());
-    } else {
+    else 
     	sorted.push(arr2.shift());
-    }
-  }
-  
-  return sorted.concat(arr1.slice().concat(arr2.slice()));
-}
+    end
+  end
 
-console.log(mergeSort(unsortedArr))
+  return sorted.concat(arr1.slice(0,arr1.length).concat(arr2.slice(0,arr2.length)));
+end
+
+puts mergeSort(unsortedArr)
